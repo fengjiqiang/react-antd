180c5ebc9ca558d76c893ec6a4a6748d6fa78e9f
Ethan Feng <fengjiqiang@inspur.com>
2021-02-17 18:58:08 +0800

    antd&eslint安装配置

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// 创建“外壳”组件App
+import React, { Component } from 'react'
+import { Button } from 'antd'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 创建并暴露App组件
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>hello antd</h2>
+        <Button type="primary">Button</Button>
+      </div>
+    )
+  }
 }
-
-export default App;
