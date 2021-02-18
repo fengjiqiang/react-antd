0cd8835c269ac4abb0eee5faf6d792ad845f0e78
Ethan Feng <fengjiqiang@inspur.com>
2021-02-18 13:22:26 +0800

    依赖包安装

---
+++ craco.config.js
@@ -0,0 +1,5 @@
+// craco.config.js
+// 修改默认配置
+module.exports = {
+  // ...
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 // 创建“外壳”组件App
 import React, { Component } from 'react'
-import { Button } from 'antd'
+import { Button, DatePicker, version } from 'antd'
 import './App.css'
 
 // 创建并暴露App组件
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h2>hello antd</h2>
-        <Button type="primary">Button</Button>
+      <div style={{ textAlign: 'center' }}>
+        <h1>
+          antd version:
+          {version}
+        </h1>
+        <DatePicker />
+        <Button type="primary" style={{ marginLeft: 8 }}>
+          Primary Button
+        </Button>
       </div>
     )
   }
